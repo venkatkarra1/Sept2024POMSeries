6ecfdb04e926a6c0b3c60a512b0617393c1beeb1
venkatkarra-qa <venkatraman.karra@gmail.com>
2025-03-03 05:27:36 +0530

    naveen employee code

--- src/main/java/com/qa/opencart/pages/EmployeePage.java
+++ src/main/java/com/qa/opencart/pages/EmployeePage.java
@@ -2,9 +2,9 @@ package com.qa.opencart.pages;
 
 public class EmployeePage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	int age = 10;
 
+	public void login() {
+		System.out.println("naveen - 10");
 	}
-
 }
